9d61c17243021df60b2cd154f69dafa03864fd6b
jzhang <jeffrey_zhang@berkeley.edu>
2014-11-28 15:48:41 -0800

    mlp hidden layer class implemented with Accelerate

--- mlp/mlp.cpp
+++ mlp/mlp.cpp
@@ -6,6 +6,40 @@
 #include <time.h>
 #include <vector>
 #include <iterator>
+#include <math.h> 
+#include <Accelerate/Accelerate.h>
+using namespace std;
+
+class Hidden_Layer {
+    /* class definition that represents a hidden layer. On construction computes the hidden layer
+     *  H(x) = s(b+Wx), where s is the activation function and b is the bias vector
+     *  Stores the result in to an output variable. 
+     *  n_in should be the #elements in input vector and #rows of layer_weights.
+     *  n_out is the number of hidden layers and #cols of layer_weights.
+     *  Each column of layer_weights corresponds to edges from input to the ith hidden layer element
+     */
+    float *input, *layer_weights, *bias, *output;
+    int n_in, n_out;
+public: 
+    Hidden_Layer(float*, float *, float *, int, int);
+
+};
+Hidden_Layer::Hidden_Layer(float *input, float *layer_weights, float *bias, int n_in, int n_out) {
+    input = input;
+    layer_weights = layer_weights;
+    bias = bias;
+    n_in = n_in;
+    n_out = n_out;
+    float *Wx = (float *) malloc(n_in*sizeof(float)); //Product Wx has dimensions n_in * 1
+    cblas_sgemv(CblasRowMajor, CblasNoTrans, n_in, n_out, 1.0f, layer_weights, n_in, input, 1, 1.0f, Wx, 1);
+    cblas_saxpy(n_in, 1.0, bias, 1, Wx, 1);
+    for (int i = 0; i < n_in; i++) {
+        Wx[i] = tanh(Wx[i]);
+        printf("%f ", Wx[i]);
+    }
+
+}
+
 int forward_prop(float *input, int input_size, float *weights, int *dim, int dim_size){
     /* @param input: array of length 36 representing 6 6x1 vectors
      * @param input_size: size of input array.
@@ -17,22 +51,17 @@ int forward_prop(float *input, int input_size, float *weights, int *dim, int dim
     int dim_index = 0;
     int weights_index = 0;
     for(dim_index; dim_index < dim_size-2; dim_index++){
-	int a = dim[dim_index];
-	int b = dim[dim_index+1];
-	float layer_weights[a][b]; //weight matrix for this layer
-	memcpy(layer_weights, weights, 36*36*sizeof(float));
-        //for(int i = 0; i<36; i++){
-	//    for(int j=0; j<36; j++){
-	//	printf("%2f ", layer_weights[i][j]);
-	//    };
-	//};
-//	std::copy(std::begin(weights), std::begin(weights+a*b), std::begin(layer_weights));
-//	printf("~~~~~~~~~~~~~~~~~~~~~\n");
-	for(int i=0; i<a; i++){
-	    for(int j=0; j<b; j++){
-//		printf("%f ", layer_weights[i][j]);
-	    }
-	}
+    	int a = dim[dim_index];
+    	int b = dim[dim_index+1];
+    	float *layer_weights = (float *) malloc(a*b*sizeof(float)); //weight matrix for this layer
+    	memcpy(layer_weights, weights, 36*36*sizeof(float));
+        float *result = (float *) malloc(36*sizeof(float));
+        float *bias = (float *) malloc(36*sizeof(float));
+
+        Hidden_Layer layer(input, layer_weights, bias, 36, 36);
+    // Need a matrix-vector multiply here. Layer_weights*input 
+    //sgemm("n","n", 36, 1, 36, 1, layer_weights, 36, input, 1, 1, result, 36);
+
     };
 };
 
@@ -72,16 +101,16 @@ int main(int argc, char* argv[]){
     float* weights = (float*) malloc(36*36*sizeof(float));
     srand (time(NULL));
     for(int i=0; i<36; i++){
-	for(int j=0; j<36; j++){
+	   for(int j=0; j<36; j++){
 	    weights[i*36+j] = float(float(rand()%10)/float(10));
-	}
+	   }
     }
 
     //fread(weights, sizeof(float), 36*36, pFile);
     //fclose(pFile);
        
     int dim[3] = {36, 36, 1};
-    float input[36];
+    float input[36] = {1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0};
     forward_prop(input, 36, weights, dim, 3);
 //    for(int i = 0; i<36*36; i++){
 //	printf("%f ", weights[i]);
