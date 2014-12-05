25b047ff582ba92fc9a859b493a7796c8e2fd48e
jzhang <jeffrey_zhang@berkeley.edu>
2014-12-04 20:43:20 -0800

    mlp progress

--- mlp/mlp.cpp
+++ mlp/mlp.cpp
@@ -18,9 +18,9 @@ class Hidden_Layer {
      *  n_out is the number of hidden layers and #cols of layer_weights.
      *  Each column of layer_weights corresponds to edges from input to the ith hidden layer element
      */
+ public: 
     float *input, *layer_weights, *bias, *output;
     int n_in, n_out;
-public: 
     Hidden_Layer(float*, float *, float *, int, int);
 
 };
@@ -34,6 +34,7 @@ Hidden_Layer::Hidden_Layer(float *input, float *layer_weights, float *bias, int 
     float *Wx = (float *) malloc(n_in*sizeof(float)); //Product Wx has dimensions n_in * 1
     cblas_sgemv(CblasRowMajor, CblasNoTrans, n_in, n_out, 1.0f, layer_weights, n_in, input, 1, 1.0f, Wx, 1);
     cblas_saxpy(n_in, 1.0, bias, 1, Wx, 1);
+
     for (int i = 0; i < n_in; i++) {
         Wx[i] = tanh(Wx[i]);
         printf("%f ", Wx[i]);
@@ -52,23 +53,29 @@ int forward_prop(float *input, int input_size, float *weights, int *dim, int dim
      */
     int dim_index = 0;
     int weights_index = 0;
-    for(dim_index; dim_index < dim_size-2; dim_index++){
+    float *bias = (float *) calloc(36, sizeof(float));
+    for(; dim_index < dim_size-2; dim_index++){
     	int a = dim[dim_index];
     	int b = dim[dim_index+1];
+        int c = dim[dim_index+2];
     	float *layer_weights = (float *) malloc(a*b*sizeof(float)); //weight matrix for this layer
+        float *sec_layer_weights = (float *) malloc(b*c*sizeof(float));
     	memcpy(layer_weights, weights, 36*36*sizeof(float));
-        float *bias = (float *) malloc(36*sizeof(float));
+        memcpy(sec_layer_weights, weights+a*b, b*c*sizeof(float));
         Hidden_Layer layer(input, layer_weights, bias, 36, 36);
-    // Need a matrix-vector multiply here. Layer_weights*input 
-    //sgemm("n","n", 36, 1, 36, 1, layer_weights, 36, input, 1, 1, result, 36);
-
     };
+    //Do final layer computation here
 };
 
 
+void softmax(float *input) {
+    /**
+     * Softmax function computes softmax of given vector.
+     * @param input: the vector to compute on. This would be the intermediate result of the last hidden layer b+Wx.
+     */
 
 
-
+}
 
 int init(){
     // create weights
