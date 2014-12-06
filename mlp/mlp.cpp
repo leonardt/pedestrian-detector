571262d6d0fa1d9047655e8384e1acc3a39bd6e0
jzhang <jeffrey_zhang@berkeley.edu>
2014-12-05 20:26:33 -0800

    mlp forward propogation done

--- mlp/mlp.cpp
+++ mlp/mlp.cpp
@@ -13,7 +13,10 @@
 #include <clapack.h>		    //C lapack
 #endif
 using namespace std;
-
+class Hidden_Layer; 
+void softmax(float *input, float *output, int n);
+int forward_prop(float *input, int input_size, Hidden_Layer* hiddenlayers, int numlayers, float* output);
+void init(int numlayers, int* layer_sizes, Hidden_Layer* hiddenlayers); 
 class Hidden_Layer {
     /* class definition that represents a hidden layer. On construction computes the hidden layer
      *  H(x) = s(b+Wx), where s is the activation function and b is the bias vector
@@ -23,31 +26,40 @@ class Hidden_Layer {
      *  Each column of layer_weights corresponds to edges from input to the ith hidden layer element
      */
  public: 
-    float *input, *layer_weights, *bias, *output;
+    float *layer_weights, *bias, *output;
     int n_in, n_out;
-    Hidden_Layer(float*, float *, float *, int, int);
+    Hidden_Layer(){};
+    Hidden_Layer(float *, float *, int, int);
+    void compute_output(float* input, int last_layer);
 
 };
-Hidden_Layer::Hidden_Layer(float *input, float *layer_weights, float *bias, int n_in, int n_out) {
-    input = input;
-    layer_weights = layer_weights;
-    bias = bias;
-    n_in = n_in;
-    n_out = n_out;
-    output = (float *) malloc(n_in*sizeof(float));
-    float *Wx = (float *) malloc(n_in*sizeof(float)); //Product Wx has dimensions n_in * 1
-    cblas_sgemv(CblasRowMajor, CblasNoTrans, n_in, n_out, 1.0f, layer_weights, n_in, input, 1, 1.0f, Wx, 1);
-    cblas_saxpy(n_in, 1.0, bias, 1, Wx, 1);
-
-    for (int i = 0; i < n_in; i++) {
-        Wx[i] = tanh(Wx[i]);
-        printf("%f ", Wx[i]);
+Hidden_Layer::Hidden_Layer(float* weights, float* b, int in, int out) {
+    layer_weights = weights;
+    bias = b;
+    n_in = in;
+    n_out = out;
+    output = (float *) malloc(n_out*sizeof(float));
+}
+void Hidden_Layer::compute_output(float* input, int last_layer) {
+    printf("nout:%d, nin:%d, input[0]: %f", n_out, n_in, input[0]);
+    if (n_out != 1) 
+        cblas_sgemv(CblasRowMajor, CblasNoTrans, n_out, n_in, 1.0f, layer_weights, n_out, input, 1, 1.0f, output, 1); //computes Wx
+    else 
+        output[0] = cblas_sdot(n_in, layer_weights, 1, input, 1);
+        printf("output is %f \n", output[0]);
+    cblas_saxpy(n_out, 1.0, bias, 1, output, 1);
+    for (int i = 0; i < n_out; i++) {
+        if (!last_layer)
+            output[i] = tanh(output[i]);
+        else {
+            softmax(output, output, n_out);
+        }
+        printf("%f ", output[i]);
     }
-    output = Wx;
-
 }
 
-int forward_prop(float *input, int input_size, float *weights, int *dim, int dim_size){
+
+int forward_prop(float *input, int input_size, Hidden_Layer* hiddenlayers, int numlayers, float* output) {
     /* @param input: array of length 36 representing 6 6x1 vectors
      * @param input_size: size of input array.
      * @param weights: weight vector
@@ -55,20 +67,12 @@ int forward_prop(float *input, int input_size, float *weights, int *dim, int dim
      * 	       	   dim[dim_size-1] is 1 for our binary classifier.
      * @param sim_size: size of dim array
      */
-    int dim_index = 0;
-    int weights_index = 0;
-    float *bias = (float *) calloc(36, sizeof(float));
-    for(; dim_index < dim_size-2; dim_index++){
-    	int a = dim[dim_index];
-    	int b = dim[dim_index+1];
-        int c = dim[dim_index+2];
-    	float *layer_weights = (float *) malloc(a*b*sizeof(float)); //weight matrix for this layer
-        float *sec_layer_weights = (float *) malloc(b*c*sizeof(float));
-    	memcpy(layer_weights, weights, 36*36*sizeof(float));
-        memcpy(sec_layer_weights, weights+a*b, b*c*sizeof(float));
-        Hidden_Layer layer(input, layer_weights, bias, 36, 36);
-    };
-    //Do final layer computation here
+     hiddenlayers[0].compute_output(input, 0);
+     for (int i = 1; i < numlayers-2; i++) {
+        hiddenlayers[i].compute_output(hiddenlayers[i-1].output, 0);
+     }
+     hiddenlayers[numlayers-2].compute_output(hiddenlayers[numlayers-3].output, 1);
+
 };
 
 
@@ -83,57 +87,71 @@ void softmax(float *input, float *output, int n) {
      */
     float denom = 0.0;
     for (int i=0; i<n; i++){
-	denom += exp(input[i]);
+	   denom += exp(input[i]);
     }
     for(int i=0; i<n; i++){
-	output[i] = exp(input[i]) / denom;
+	   output[i] = exp(input[i]) / denom;
     }
 }
 
-int init(){
-    // create weights
-    //float weights[36][36] = {};
-    float* weights = (float*) malloc(36*36*sizeof(float));
-    srand (time(NULL));
-    for(int i=0; i<36; i++){
-	for(int j=0; j<36; j++){
-	    weights[i*36+j] = float(float(rand()%10)/float(10));
-	}
+void init(int numlayers, int* layer_sizes, Hidden_Layer* hiddenlayers) {
+    /* Initializes weights and hidden layers 
+     * @param numlayers is the total number of layers. Number of hidden layers is numlayers - 2
+     * @param layer_sizes is an int array of size numlayers that stores the sizes of each layer. 
+     * weights are initialized randomly or read in from an existing file. 
+     */
+    int weights_size = 0;
+    int bias_size = 0;
+    for (int k = 0; k < numlayers-1; k++) {
+        weights_size += layer_sizes[k]*layer_sizes[k+1];
+        bias_size += layer_sizes[k];
     }
-    FILE *pFile = fopen("weights", "wb");
-    if (!pFile) {
-	std::cout<<"error opening file";
-	exit(0);
-    };
-    fwrite(weights, sizeof(float), 36*36, pFile);
-    fclose(pFile); 
+    bias_size -= layer_sizes[numlayers-1];
+    float* weights = (float*) malloc(weights_size*sizeof(float));
+    float* bias = (float*) malloc(bias_size*sizeof(float));
+    srand (time(NULL));
+    for (int i=0; i<weights_size; i++){
+	    //weights[i] = float(float(rand()%10)/float(10));
+        weights[i] = 0.5;
+    }
+    for (int i = 0; i < bias_size; i++) {
+        //bias[x] = float(float(rand()%10)/float(10));
+        bias[i] = 0.5;
+    }
+    //Hidden_Layer* hiddenlayers = new Hidden_Layer[numlayers-1];
+    int weights_offset = 0;
+    int bias_offset = 0;
+    for (int j = 0; j < numlayers-1; j++) {
+        Hidden_Layer temp(weights+weights_offset, bias+bias_offset, layer_sizes[j], layer_sizes[j+1]);
+        printf("weights: %f ", *(weights+weights_offset));
+
+        printf("temp layer: %f \n", temp.layer_weights[0]);
+        hiddenlayers[j] = temp;
+        weights_offset += layer_sizes[j] * layer_sizes[j+1];
+        bias_offset += layer_sizes[j];
+    }
+
+
+
+ //    FILE *pFile = fopen("weights", "wb");
+ //    if (!pFile) {
+	// std::cout<<"error opening file";
+	// exit(0);
+ //    };
+ //    fwrite(weights, sizeof(float), 36*36, pFile);
+ //    fclose(pFile); 
 };
 
 int main(int argc, char* argv[]){
-    // initialize weights if it wasn't already 
-    // init(...)
-    //FILE *pFile = fopen("weights", "rb");
-    //if (!pFile) {
-//	std::cout<<"error opening file";
-//	exit(0);
- //   };
-    //float weights[36][36];
-    float* weights = (float*) malloc(36*36*sizeof(float));
-    srand (time(NULL));
-    for(int i=0; i<36; i++){
-	   for(int j=0; j<36; j++){
-	    weights[i*36+j] = float(float(rand()%10)/float(10));
-	   }
-    }
 
+    Hidden_Layer* hiddenlayers = new Hidden_Layer[2];
+    int layer_sizes[3] = {36, 36, 1};
+    init(3,layer_sizes, hiddenlayers);
     //fread(weights, sizeof(float), 36*36, pFile);
     //fclose(pFile);
-       
-    int dim[3] = {36, 36, 1};
-    float input[36] = {1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0};
-    forward_prop(input, 36, weights, dim, 3);
-//    for(int i = 0; i<36*36; i++){
-//	printf("%f ", weights[i]);
- //   };
-    //{int forward_prop(float *input, int input_size, float *weights, int *dim, int dim_size){
+    float output[1];
+    float input[36] = {0.01, 0.01, 0.01, 0.01, 0.01, 0.01, 0.01, 0.01, 0.01, 0.01, 0.01, 0.01, 0.01, 0.01, 0.01, 0.01, 0.01, 0.01, 0.01, 0.01, 0.01, 0.01, 0.01, 0.01, 0.01, 0.01, 0.01, 0.01, 0.01, 0.01, 0.01, 0.01, 0.01, 0.01, 0.01, 0.01};
+
+    forward_prop(input, 36, hiddenlayers, 3, output);
+
 };
