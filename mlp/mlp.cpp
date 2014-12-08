b19f4f8202aa00ca47369f98e08cafcec088717c
James Wong <jmswong12@gmail.com>
2014-12-07 22:20:45 -0800

    bp

--- mlp/mlp.cpp
+++ mlp/mlp.cpp
@@ -47,22 +47,22 @@ Hidden_Layer::Hidden_Layer(float* weights, float* b, int in, int out) {
     v = (float *) malloc(n_out*sizeof(float)); 
 }
 void Hidden_Layer::compute_output(float* input, int last_layer) {
-    printf("nout:%d, nin:%d \n", n_out, n_in);
+    //printf("nout:%d, nin:%d \n", n_out, n_in);
     cblas_sgemv(CblasRowMajor, CblasNoTrans, n_out, n_in, 1.0f, layer_weights, n_in, input, 1, 1.0f, output, 1); //computes Wx
-    printf("should be 10.647... vector\n");
+    //printf("should be 10.647... vector\n");
     for(int i=0; i<n_out; i++){
-	printf("output[i] = %f\n", output[i]);
+	//printf("output[i] = %f\n", output[i]);
     }
-    printf("bias = [%f, %f]\n", bias[0], bias[1]);
-    printf("OUTPUT = [%f, %f]\n", output[0], output[1]);
+    //printf("bias = [%f, %f]\n", bias[0], bias[1]);
+    //printf("OUTPUT = [%f, %f]\n", output[0], output[1]);
     cblas_saxpy(n_out, 1.0, bias, 1, output, 1);
-    printf("OUTPUT = [%f, %f]\n", output[0], output[1]);
-    printf("v vector for layer\n");
+    //printf("OUTPUT = [%f, %f]\n", output[0], output[1]);
+    //printf("v vector for layer\n");
     if (!last_layer) {
 	for (int i = 0; i < n_out; i++) {
 	    v[i] = output[i];
-	    printf("OUTPUT = [%f, %f]\n", output[0], output[1]);
-	    printf("v[%d] = %f\n", i, v[i]);
+	    //printf("OUTPUT = [%f, %f]\n", output[0], output[1]);
+	    //printf("v[%d] = %f\n", i, v[i]);
 	    output[i] = tanh(output[i]);
 	}
     } else {
@@ -71,7 +71,7 @@ void Hidden_Layer::compute_output(float* input, int last_layer) {
         }
 	softmax(output, output, n_out);
     }
-    printf("OUTPUT = [%f, %f]\n", output[0], output[1]);
+    //printf("OUTPUT = [%f, %f]\n", output[0], output[1]);
 }   
 
 
@@ -93,7 +93,7 @@ void forward_prop(float *input, int input_size, Hidden_Layer* hiddenlayers, int 
 void backprop(float* input, int input_size, Hidden_Layer* hiddenlayers, int numlayers, float* actual) {
     float cost1 = cost(input, input_size, hiddenlayers, numlayers, actual);
     int largest_layer_size = hiddenlayers[0].n_in; //36
-    int layer_sizes[numlayers]; // [36, 36, 2]
+    int layer_sizes[numlayers]; // [36, 2]
     layer_sizes[0] = hiddenlayers[0].n_in;
     for(int i=0; i<numlayers-1; i++){
 	largest_layer_size = max(largest_layer_size, hiddenlayers[numlayers-i-2].n_out);
@@ -107,12 +107,12 @@ void backprop(float* input, int input_size, Hidden_Layer* hiddenlayers, int numl
     int output_size = hiddenlayers[numlayers-2].n_out;
     float activation_partial[output_size];
     softmax_prime(hiddenlayers[numlayers-2].v, activation_partial, output_size); // softmax'(Wx+b)
-    printf("softmaxprime      input 	output: \n");
+    printf("softmaxprime      input 	output:       layer 1\n");
     printf("	0 		%f 	   %f    \n", hiddenlayers[numlayers-2].v[0], activation_partial[0]);  
     printf("	1 		%f 	   %f    \n", hiddenlayers[numlayers-2].v[1], activation_partial[1]);  
     for(int i=0; i<hiddenlayers[numlayers-2].n_out; i++) {
 	   errors[(numlayers-2)*largest_layer_size+i] -= actual[i]; //a_L - y [-.5, .5]
-	   //printf("a_L - y for layer %d index %d = %f \n",numlayers-2, i, errors[numlayers-2][i]); 
+	   ////printf("a_L - y for layer %d index %d = %f \n",numlayers-2, i, errors[numlayers-2][i]); 
 	   errors[(numlayers-2)*largest_layer_size+i] *= activation_partial[i]; // errors[1] is output layer errors
     }
     // next compute error for all layers
@@ -120,46 +120,49 @@ void backprop(float* input, int input_size, Hidden_Layer* hiddenlayers, int numl
     for(int i = numlayers-2; i>0; i--) { //only does i=1
 	// first find (w_i+1)^T * errors[i]
 	for(int j = 0; j < 2; j++) { // 0 to 36
-	    printf("INPUT = %f\n", errors[i*largest_layer_size+j]);
+	    //printf("INPUT = %f\n", errors[i*largest_layer_size+j]);
 	}
 	for(int j = 0; j < 72; j++) { // 0 to 36
-	    printf("LAYER_WEIGHTS_INPUT = %f\n", hiddenlayers[i].layer_weights[j]);
+	    //printf("LAYER_WEIGHTS_INPUT = %f\n", hiddenlayers[i].layer_weights[j]);
 	}
+//	float errors1[2] = {0.0, 0.0};
+//	float *input1 = (float *) malloc(72*sizeof(float));
+//	float *output2 = (float *)malloc(36*sizeof(float));
 
-	float errors1[2] = {0.0, 0.0};
-    float *input1 = (float *) malloc(72*sizeof(float));
-    float *output2 = (float *)malloc(36*sizeof(float));
-
-     for (int i = 0; i<72; i++) {
-         input1[i] = 0.5;
-     }
-    cblas_sgemv(CblasRowMajor, CblasTrans, 
-        2, // M
-        36, // N
-        1.0f, //
-        input1,
-        36,
-        errors+largest_layer_size,
-        1,
-        1.0f,
-        //errors[i-1], // output
-        errors, // output
-        1);
-    for(int j = 0; j < 36; j++) { // 0 to 36
-        printf("AFTER errors[%d][%d] = %f\n", 0, j, output2[j]);
-    }
+//	for (int i = 0; i<72; i++) {
+//	    input1[i] = 0.5;
+//	}
+	cblas_sgemv(CblasRowMajor, CblasTrans, 
+		2, // M
+		36, // N
+		1.0f, //
+		hiddenlayers[i].layer_weights,
+		36,
+		errors+largest_layer_size,
+		1,
+		1.0f,
+		//errors[i-1], // output
+		errors, // output
+		1);
+	for(int j = 0; j < 36; j++) { // 0 to 36
+	    //printf("AFTER errors[%d][%d] = %f\n", 0, j, errors[j]);
+	}
 	for(int j = 0; j < hiddenlayers[i].n_in; j++) { // 0 to 36
-	    //printf("AFTER errors[%d][%d] = %f\n", i-1, j, output2[j]);
+	    ////printf("AFTER errors[%d][%d] = %f\n", i-1, j, output2[j]);
 	    //errors[i-1][j] *= tanh_prime(hiddenlayers[i-1].v[j]);
-	    errors[(i-1)*largest_layer_size + j] = output2[j] * tanh_prime(hiddenlayers[i-1].v[j]);
+	    errors[(i-1)*largest_layer_size + j] = errors[j] * tanh_prime(hiddenlayers[i-1].v[j]);
 	}
     }
-    // printf( "output errors: \n");
-    // printf( "errors[1][0] = %f \n", errors[1][0]);
-    // printf( "errors[1][1] = %f \n", errors[1][1]);
-    
+    printf("softmaxprime      input 	output:        layer 0\n");
+    printf("	0 		%f 	       \n", hiddenlayers[numlayers-3].v[0]);  
+    printf("	1 		%f 	       \n", hiddenlayers[numlayers-3].v[1]);  
 
-
+    // partial derivitive of C wrt to bias is just error.
+    // compute partial derivative of C wrt weights. d = a_(l-1) * error_l
+    float* partialW = malloc(
+    for(int i=numlayers-2; i>=0; i--) {
+	
+    }
 
 }
 
@@ -244,7 +247,7 @@ void init(int numlayers, int* layer_sizes, Hidden_Layer* hiddenlayers) {
     float* bias = (float*) malloc(bias_size*sizeof(float));
     srand (time(NULL));
     for (int i=0; i<weights_size; i++){
-	    //weights[i] = float(float(rand()%10)/float(10));
+	//weights[i] = float(float(rand()%10)/float(10));
         weights[i] = 0.5;
     }
     for (int i = 0; i < bias_size; i++) {
@@ -256,18 +259,18 @@ void init(int numlayers, int* layer_sizes, Hidden_Layer* hiddenlayers) {
     int bias_offset = 0;
     for (int j = 0; j < numlayers-1; j++) {
         Hidden_Layer temp(weights+weights_offset, bias+bias_offset, layer_sizes[j], layer_sizes[j+1]);
-        printf("weights: %f ", *(weights+weights_offset));
-        printf("temp layer: %f \n", temp.layer_weights[0]);
+        //printf("weights: %f ", *(weights+weights_offset));
+        //printf("temp layer: %f \n", temp.layer_weights[0]);
         hiddenlayers[j] = temp;
         weights_offset += layer_sizes[j] * layer_sizes[j+1];
         bias_offset += layer_sizes[j];
     }
-    printf("\n hiddenlayer[1] weights: \n");
+    //printf("\n hiddenlayer[1] weights: \n");
     for(int i=0; i<72; i++) {
-	printf("hiddenlayer[1].weights[%d] = %f\n", i, hiddenlayers[1].layer_weights[i]);
+	//printf("hiddenlayer[1].weights[%d] = %f\n", i, hiddenlayers[1].layer_weights[i]);
     }
-    printf("bias\n");
-    printf("bias[0] = %f\nbias[1] = %f", hiddenlayers[1].bias[0], hiddenlayers[1].bias[1]);
+    //printf("bias\n");
+    //printf("bias[0] = %f\nbias[1] = %f", hiddenlayers[1].bias[0], hiddenlayers[1].bias[1]);
     //    FILE *pFile = fopen("weights", "wb");
     //    if (!pFile) {
 	// std::cout<<"error opening file";
@@ -282,7 +285,7 @@ void testsoftmaxprime() {
     float output1[36];
     softmax_prime(input, output1, 36);
     for (int i = 0; i < 36; i++) {
-        //printf("%f   ", output1[i]);
+        ////printf("%f   ", output1[i]);
     }
 }
 void testLoss() {
@@ -291,17 +294,17 @@ void testLoss() {
     float output[2] = {2, 2};
     float l = loss(input, output, 2); // should be (3-2) + (3-2) = 2
     assert(l==2.0);
-    //printf("loss = %f \n", l);
+    ////printf("loss = %f \n", l);
     float input2[2] = {5.5, 6.5};
     float output2[2] = {3.0, 0.0};
     float l2 = loss(input2, output2, 2); // should be (5.5-3) + (6.5-0) = 9
-    //printf("loss = %f \n", l2);
+    ////printf("loss = %f \n", l2);
     assert(l2==9.0);
 }
 
 void testCost(Hidden_Layer* hiddenlayers) {
     //test the cost function with a simple model
-    float input[36] = {0.01, 0.01, 0.01, 0.01, 0.01, 0.01, 0.01, 0.01, 0.01, 0.01, 0.01, 0.01, 0.01, 0.01, 0.01, 0.01, 0.01, 0.01, 0.01, 0.01, 0.01, 0.01, 0.01, 0.01, 0.01, 0.01, 0.01, 0.01, 0.01, 0.01, 0.01, 0.01, 0.01, 0.01, 0.01, 0.01};
+    float input[36] = {100.0, 333.33, 333.55, 0.01, 0.01, 0.01, 0.01, 0.01, 0.01, 0.01, 0.01, 0.01, 0.01, 0.01, 0.01, 0.01, 0.01, 0.01, 0.01, 0.01, 0.01, 0.01, 0.01, 0.01, 0.01, 0.01, 0.01, 0.01, 0.01, 0.01, 0.01, 0.01, 0.01, 0.01, 0.01, 0.01};
     float output[2] = {1.0 ,1.0};
     // cost1 should be .5*[(output[0] - model[0]) + (output[1] - model[1])] = .5*[(1-.5) + (1-.5)] = 0.5
     float cost1 = cost(input, 36, hiddenlayers, 3, output); 
@@ -317,13 +320,13 @@ int main(int argc, char* argv[]){
     //fread(weights, sizeof(float), 36*36, pFile);
     //fclose(pFile);
     float* output;
-    float input[36] = {0.01, 0.01, 0.01, 0.01, 0.01, 0.01, 0.01, 0.01, 0.01, 0.01, 0.01, 0.01, 0.01, 0.01, 0.01, 0.01, 0.01, 0.01, 0.01, 0.01, 0.01, 0.01, 0.01, 0.01, 0.01, 0.01, 0.01, 0.01, 0.01, 0.01, 0.01, 0.01, 0.01, 0.01, 0.01, 0.01};
+    float input[36] = {333.0, 333.01, 0.01, 0.01, 0.01, 0.01, 0.01, 0.01, 0.01, 0.01, 0.01, 0.01, 0.01, 0.01, 0.01, 0.01, 0.01, 0.01, 0.01, 0.01, 0.01, 0.01, 0.01, 0.01, 0.01, 0.01, 0.01, 0.01, 0.01, 0.01, 0.01, 0.01, 0.01, 0.01, 0.01, 0.01};
     //testsoftmaxprime();
 
     //cblas_sgemv(CblasRowMajor, CblasNoTrans, 2, 36, 1.0f, input2, 36, input, 1, 1.0f, output, 1);
 //    forward_prop(input, 36, hiddenlayers, 3);
     output = hiddenlayers[1].output;
-    printf("%f  %f \n", output[0], output[1]);
+    //printf("%f  %f \n", output[0], output[1]);
 
     //void backprop(float* input, int input_size, Hidden_Layer* hiddenlayers, int numlayers, float* actual) {
     float realoutput[2] = { 1, 0 };
@@ -366,7 +369,7 @@ int main(int argc, char* argv[]){
 	//     1);
 
  //    for(int i=0; i<36; i++){
-	// printf("output2[%d] = %f\n", i, output2[i]);
+	// //printf("output2[%d] = %f\n", i, output2[i]);
  //    }
 
 };
